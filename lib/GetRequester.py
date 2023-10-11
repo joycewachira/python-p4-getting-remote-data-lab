5830634c1b556a9ae8bff8fee051b2d7c96d4092
Joyce <joycewachira718@gmail.com>
2023-10-11 20:43:40 +0300

    Completed lab

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,22 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+        try:
+            response = requests.get(self.url)
+            response.raise_for_status()
+            return response.content
+        except requests.exceptions.RequestException as e:
+            print(f"Request failed: {e}")
+            return None
 
     def load_json(self):
-        pass
+        response_body = self.get_response_body()
+        if response_body:
+            try:
+                json_data = json.loads(response_body.decode('utf-8'))
+                return json_data
+            except json.JSONDecodeError as e:
+                print(f"JSON decoding failed: {e}")
+                return None
+        else:
+            return None
